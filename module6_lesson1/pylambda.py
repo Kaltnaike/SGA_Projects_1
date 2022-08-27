cd07e0de199dca0dfb40c862375cf9be0c0c51b0
Kaltnaike <purpleduralumin@gmail.com>
2022-08-27 13:25:45 +0100

    Using python lambda with map(),filter().

--- module6_lesson1/pylambda.py
+++ module6_lesson1/pylambda.py
@@ -1,12 +1,13 @@
 #Create a list between 5.5 and 20.5
-nums = [5.5,6.0,6.5,7.0,7.5,8.0,8.5,9.0,9.5,10.0,10.5,11.0,11.5,12.0,12.5,13.0,13.5,14.0,14.5,15.0,15.5,16.0,16.5,17.0,17.5,18.0,18.5,19.0,19.5,20.0,20.5]
-print("Original list of Floats:")
+nums = [6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print("\nOriginal list of Integers:")
 print(nums)
 
+
 #Using the lambda function count the even and odd numbers in the list
-even_nums = list(filter(lambda x: (x % 2 == 0), nums))
+even_nums =list(filter(lambda x: (x % 2 == 0), nums))
 odd_nums = list(filter(lambda x: (x % 2 != 0), nums))
-print("{0} even numbers and {1} odd numbers in the nums list.".format(len(even_nums), len(odd_nums)))
+print("{0} even numbers and {1} odd numbers in the Original List of Integers.".format(len(even_nums), len(odd_nums)))
 
 #Using the lambda function that square and cube every number in the list
 print("\nSquare the numbers in the list:")
